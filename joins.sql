3febf856dabaf8b858f6fadc08f54846e6f57af8
buffyhridoy <chemicalbuffy@gmail.com>
2024-10-25 06:54:39 +0600

    LEFT JOIN done

--- joins.sql
+++ joins.sql
@@ -5,16 +5,20 @@ SELECT * FROM manager;
 
 
 -- INNER JOIN
--- Fetch the employee name and the department name they belong to.
- -- Whenever we are trying to join two tables we can not just specify the two table that I need to JOIN. Most important thing is we need to specify the condition on which we need to join these two table. We need to join two table base on some column. Find the common column that are present in both that column. It does not matter if the column names are same or not, the value has to be similar.
+ -- Fetch the employee name and the department name they belong to.
+  -- Whenever we are trying to join two tables we can not just specify the two table that I need to JOIN. Most important thing is we need to specify the condition on which we need to join these two table. We need to join two table base on some column. Find the common column that are present in both that column. It does not matter if the column names are same or not, the value has to be similar.
 
 SELECT e.emp_name, d.department_name
 FROM employee e
 JOIN department d ON e.dept_id = d.department_id;
 
 
-
-
+-- LEFT JOIN
+ -- Fetch ALL the employee name and their department name they belong to.
+  -- Whenever we have a requirement where we need to fetch a record from a table which may not join with the other table in that case this is the best way to use either LEFT JOIN or RIGHT JOIN.
+SELECT e.emp_name, d.department_name
+FROM employee e
+LEFT JOIN department d ON e.dept_id = d.department_id;
 
 
 
